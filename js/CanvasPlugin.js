442927a28ca574b7fd220d67ed0e306371e9d096
Mauricio alfaro <mauricio-alfaro@hotmail.com>
2013-05-07 17:18:24 -0600

    canvas

---
+++ js/CanvasPlugin.js
@@ -0,0 +1,20 @@
+window.canvasplugin = function(canvasEl,callback){
+	var canvasProps = {
+		mimeType: "image/png", //this is hard coded right now, change it as per your need
+		xpos: canvasEl.offsetLeft,
+		ypos: canvasEl.offsetTop,
+		width: canvasEl.width,
+		height: canvasEl.height,
+		screenWidth: window.innerWidth // no WebView.getContentWidth(), use this instead
+	};
+
+	//call the Plugin execute method()
+	cordova.exec(callback,function(err){
+		callback('Error: ' + err);	
+	},"CanvasPlugin","toDataURL",[canvasProps.mimeType,
+							canvasProps.xpos,
+							canvasProps.ypos,
+							canvasProps.width,
+							canvasProps.height,
+							canvasProps.screenWidth]);	
+}
